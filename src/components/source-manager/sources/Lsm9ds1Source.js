49f055eb2fee1e4e1677c96e3bd9e2a0b5b85ca8
Mael <maellelievre31@gmail.com>
2026-05-16 15:41:18 +0200

    Comments in English and camelCase notation

--- src/components/source-manager/sources/Lsm9ds1Source.js
+++ src/components/source-manager/sources/Lsm9ds1Source.js
@@ -2,74 +2,73 @@ import AbstractSource from './AbstractSource.js';
 import { getTime } from '@ircam/sc-utils';
 
 
-// Import conditionnel : i2c n'existe que sur Linux
-// Sur Mac, on n'importe pas le module pour ne pas faire planter le serveur
+// Conditional import: i2c is only available on Linux
+// On Mac, we don't import the module to prevent the server from crashing
 let I2C = null;
 if (process.platform === 'linux') {
   const module = await import('i2c');
   I2C = module.default;
 }
 
+// --------- I2C Address ----------
+const LSM9DS1_GYRO_ACCEL_ADDRESS = 0x6B; // I2C address of the gyroscope and accelerometer
+const LSM9DS1_MAGNETO_ADDRESS    = 0x1E; // I2C address of the magnetometer
 
-// ------------ Adresses I2C du gyroscope/accéléromètre et du magnétomètre -----------------
-const LSM9DS1_GYRO_ACCEL_ADDRESS = 0x6B; // Adresse I2C du gyroscope et de l'accéléromètre
-const LSM9DS1_MAGNETO_ADDRESS       = 0x1E; // Adresse I2C du magnétomètre
 
-
+// ------- Gyroscope/Accelerometer Registers --------
 const GYRO_ACCEL_REGISTER = {
-  //------ Registres généraux ----
-  WHO_AM_I:     0x0F, // Registre d'identification du gyroscope/accéléromètre (== 0x68 par default)
-  CTRL_REG8:    0x22, // Auto-incrémentation des adresses
 
-  // --- Registre du gyroscope -----
-  CTRL_REG1_G:  0x10, // ODR (Hz), Précision gyroscope (dps)
-  OUT_X_G:      0x18, // Premier registre de sortie du gyroscope (OUT_X_L_G / X Low)
+  WHO_AM_I:     0x0F, // Gyroscope/Accelerometer identification register (== 0x68 by default)
+  CTRL_REG8:    0x22, // Address auto-increment
 
-  // ---- Registre de l'accéléromètre ---
-  CTRL_REG6_XL: 0x20, // ODR (Hz), Précision accéléromètre (g)
-  OUT_X_XL:     0x28, // Premier registre de sortie de l'accéléromètre (OUT_X_L_XL / X Low)
+  // ---  Gyroscope Register -----
+  CTRL_REG1_G:  0x10, // ODR (Hz), gyroscope scale (dps)
+  OUT_X_G:      0x18, // First gyroscope output register (OUT_X_L_G / X Low)
+
+  // ---- Accelerometer Register ---
+  CTRL_REG6_XL: 0x20, // ODR (Hz), accelerometer scale (g)
+  OUT_X_XL:     0x28, // First accelerometer output register (OUT_X_L_XL / X Low)
 };
 
 
-// --------------------- Registres du magnétomètre --------------------
+// -------- Magnetometer Registers ----------
 const MAGNETO_REGISTER = {
-  WHO_AM_I_M:   0x0F, // Registre d'identification du magnétomètre (== 0x3D par default)
-  CTRL_REG1_M:  0x20, // ODR (Hz), mode de performance XY, compensation température
-  CTRL_REG2_M:  0x21, // Précision magnétomètre (gauss)
-  CTRL_REG3_M:  0x22, // Mode continu
-  CTRL_REG4_M:  0x23, // Mode de performance axe Z
-  OUT_X_M:      0x28, // Premier registre de sortie du magnétomètre (OUT_X_L_M / X Low)
+  WHO_AM_I_M:   0x0F, // Magnetometer identification register (== 0x3D by default)
+  CTRL_REG1_M:  0x20, // ODR (Hz), XY-axis performance mode, temperature compensation
+  CTRL_REG2_M:  0x21, // Magnetometer scale (gauss)
+  CTRL_REG3_M:  0x22, // Continuous mode
+  CTRL_REG4_M:  0x23, // Z-axis performance mode
+  OUT_X_M:      0x28, // First magnetometer output register (OUT_X_L_M / X Low)
 };
 
 
 
-// ---------------------- Sensibilités des capteurs ------------------------------------
+// -------- Unit Sensor Sensitivities -----------
 const ACCEL_SENSIVITY = 0.000061; // g/LSB ---> ±2g
 const GYRO_SENSIVITY = 0.00875;     // dps/LSB --->  ±245 dps
 const MAGNETO_SENSIVITY = 0.00014; // gauss/LSB  ---->  ±4 gauss
 
 
 
-// ---------------------- Coefficients pour la conversion des unités -----------------------------------
-const GAUSS_TO_UT = 100;   // ---> 1 gauss = 0.0001 Tesla (T) soit 100 microTesla (uT)
+// --------- Conversion factors ------------
+const GAUSS_TO_UT = 100;   // ---> 1 gauss = 0.0001 Tesla (T), or 100 microTesla (uT)
 const DEG_TO_RAD = Math.PI / 180;
 const GRAV_TO_M_S_2 = 9.81;    // --->  1g = 9,81 m/s^2
 
 
-// ------ Fréquence de lecture -------
-const DEFAULT_INTERVAL_MS = 10; // Par défaut 100 Hz
+// ------ Reading Frequency -------
+const DEFAULT_INTERVAL_MS = 10; // ms ---> 100 Hz by default
 const DEFAULT_ACTIVE_TIMEOUT_MS= 500;
 
 
-//------------------------------------------------------------
 
 class Lsm9ds1Source extends AbstractSource {
   static type = 'lsm9ds1';
 
   #config = null;
 
-  #Gyro_Accel_Wire = null;
-  #Magneto_Wire = null;
+  #gyroAccelWire = null;
+  #magnetoWire = null;
   #intervalId = null;
 
   #activeTimeoutId = null;
@@ -122,7 +121,7 @@ class Lsm9ds1Source extends AbstractSource {
 
 
 
-  // ----------------------- Ouverture d'une connexion I2C -------------------------------
+  // -------- Opening an I2C connection -------------
   #openWire(address) {
     return new Promise((resolve, reject) => {
       const wire = new I2C(address, { device: '/dev/i2c-1' });
@@ -132,7 +131,7 @@ class Lsm9ds1Source extends AbstractSource {
   }
 
 
-  //--------- Lecture dans un registre ------------
+  //--------- Reading from a register ------------
   #readRegister(wire, register, length) {
     return new Promise((resolve, reject) => {
       wire.readBytes(register, length, (err, res) => {
@@ -146,7 +145,7 @@ class Lsm9ds1Source extends AbstractSource {
   }
 
 
-  //--------- Ecriture dans un registre ------------
+  //--------- Writing in a register ------------
   #writeRegister(wire, register, value) {
     return new Promise((resolve, reject) => {
       wire.writeBytes(register, [value], (err) => {
@@ -160,7 +159,7 @@ class Lsm9ds1Source extends AbstractSource {
   }
 
 
-  //---------------------- Initialisation du capteur ------------------------------
+  //------------ Sensor Initialization -------------
   async #initSensor() {
 
     if (I2C === null) {
@@ -168,37 +167,37 @@ class Lsm9ds1Source extends AbstractSource {
       return;
     }
 
-    this.#Gyro_Accel_Wire = await this.#openWire(LSM9DS1_GYRO_ACCEL_ADDRESS);
-    this.#Magneto_Wire = await this.#openWire(LSM9DS1_MAGNETO_ADDRESS);
+    this.#gyroAccelWire = await this.#openWire(LSM9DS1_GYRO_ACCEL_ADDRESS);
+    this.#magnetoWire = await this.#openWire(LSM9DS1_MAGNETO_ADDRESS);
 
 
-    // Vérification WHO_AM_I Gyroscope/Magnétomètre (doit retourner 0x68)
-    const Gyro_Accel_Id = await this.#readRegister(this.#Gyro_Accel_Wire, GYRO_ACCEL_REGISTER.WHO_AM_I, 1);
-    if (Gyro_Accel_Id[0] !== 0x68) {
-      throw new Error(`LSM9DS1 gyroscope and accelerometer not detected (WHO_AM_I=0x${Gyro_Accel_Id[0].toString(16)}, attendu 0x68)`);
+    // WHO_AM_I gyroscope/accelerometer check (should return 0x68)
+    const gyroAccelId = await this.#readRegister(this.#gyroAccelWire, GYRO_ACCEL_REGISTER.WHO_AM_I, 1);
+    if (gyroAccelId[0] !== 0x68) {
+      throw new Error(`LSM9DS1 gyroscope and accelerometer not detected (WHO_AM_I=0x${gyroAccelId[0].toString(16)}, attendu 0x68)`);
     }
     console.log('LSM9DS1 gyroscope and accelerometer identified');
 
 
-    // Vérification WHO_AM_I Magnétomètre (doit retourner 0x3D)
-    const Magneto_Id = await this.#readRegister( this.#Magneto_Wire, MAGNETO_REGISTER.WHO_AM_I_M, 1);
-    if (Magneto_Id[0] !== 0x3D) {
-      throw new Error(`LSM9DS1 magnetometer not detected (WHO_AM_I=0x${Magneto_Id [0].toString(16)}, attendu 0x3D)`);
+    // WHO_AM_I magnetometer check (should return 0x3D)
+    const magnetoId = await this.#readRegister( this.#magnetoWire, MAGNETO_REGISTER.WHO_AM_I_M, 1);
+    if (magnetoId[0] !== 0x3D) {
+      throw new Error(`LSM9DS1 magnetometer not detected (WHO_AM_I=0x${magnetoId [0].toString(16)}, attendu 0x3D)`);
     }
     console.log('LSM9DS1 Magnetometer identified');
 
 
-    // ------------ Configuration Gyroscope et Magnétomètre --------------
-    await this.#writeRegister(this.#Gyro_Accel_Wire, GYRO_ACCEL_REGISTER.CTRL_REG8, 0x04); // auto-incrémentation d'adresse
-    await this.#writeRegister(this.#Gyro_Accel_Wire, GYRO_ACCEL_REGISTER.CTRL_REG1_G, 0x80); // Gyroscope: ODR 238 Hz, ±245 dps
-    await this.#writeRegister(this.#Gyro_Accel_Wire, GYRO_ACCEL_REGISTER.CTRL_REG6_XL, 0x80);  //Accéléromètre: ODR 238 Hz, ±2g
+    // --------- Gyroscope and Magnetometer Settings ------------
+    await this.#writeRegister(this.#gyroAccelWire, GYRO_ACCEL_REGISTER.CTRL_REG8, 0x04); // Auto-incrementing address
+    await this.#writeRegister(this.#gyroAccelWire, GYRO_ACCEL_REGISTER.CTRL_REG1_G, 0x80); // Gyroscope: ODR 238 Hz, ±245 dps
+    await this.#writeRegister(this.#gyroAccelWire, GYRO_ACCEL_REGISTER.CTRL_REG6_XL, 0x80);  // Accelerometer: ODR 238 Hz, ±2g
 
 
-    // ------------ Configuration Magnétomètre --------------
-    await this.#writeRegister(this.#Magneto_Wire, MAGNETO_REGISTER.CTRL_REG1_M, 0xFC); // ultra-haute perf. XY, ODR 80 Hz, Compensation Temperature
-    await this.#writeRegister(this.#Magneto_Wire, MAGNETO_REGISTER.CTRL_REG2_M, 0x00); // ±4 gauss
-    await this.#writeRegister(this.#Magneto_Wire, MAGNETO_REGISTER.CTRL_REG3_M, 0x00); // mode continu
-    await this.#writeRegister(this.#Magneto_Wire, MAGNETO_REGISTER.CTRL_REG4_M, 0x0C); // ultra-haute perf. Z
+    // ------------ Magnetometer Settings --------------
+    await this.#writeRegister(this.#magnetoWire, MAGNETO_REGISTER.CTRL_REG1_M, 0xFC); // Ultra-high performance XY-axis, ODR 80 Hz, Temperature Compensation
+    await this.#writeRegister(this.#magnetoWire, MAGNETO_REGISTER.CTRL_REG2_M, 0x00); // ±4 gauss
+    await this.#writeRegister(this.#magnetoWire, MAGNETO_REGISTER.CTRL_REG3_M, 0x00); // Continuous mode
+    await this.#writeRegister(this.#magnetoWire, MAGNETO_REGISTER.CTRL_REG4_M, 0x0C); // Ultra-high performance Z-axis
 
 
     console.log('LSM9DS1 sensor initialized');
@@ -207,20 +206,20 @@ class Lsm9ds1Source extends AbstractSource {
 
 
 
-  //------------------ Lectures des données du capteur ------------------------
+  //------------ Reading sensor data -----------
   #readSensor = async () => {
 
-    if (this.#Gyro_Accel_Wire === null || this.#Magneto_Wire === null) {
+    if (this.#gyroAccelWire === null || this.#magnetoWire === null) {
       return;
     }
 
 
     try {
-      const Gyro_Buffer = await this.#readRegister(this.#Gyro_Accel_Wire, GYRO_ACCEL_REGISTER.OUT_X_G, 6);
-      const Accel_Buffer = await this.#readRegister(this.#Gyro_Accel_Wire, GYRO_ACCEL_REGISTER.OUT_X_XL, 6);
-      const Magneto_Buffer = await this.#readRegister(this.#Magneto_Wire, MAGNETO_REGISTER.OUT_X_M, 6);
+      const gyroBuffer = await this.#readRegister(this.#gyroAccelWire, GYRO_ACCEL_REGISTER.OUT_X_G, 6);
+      const accelBuffer = await this.#readRegister(this.#gyroAccelWire, GYRO_ACCEL_REGISTER.OUT_X_XL, 6);
+      const magnetoBuffer = await this.#readRegister(this.#magnetoWire, MAGNETO_REGISTER.OUT_X_M, 6);
 
-      const now = getTime() * 1e3; // millisecondes
+      const now = getTime() * 1e3; // ms
 
 
       const data = {
@@ -229,30 +228,30 @@ class Lsm9ds1Source extends AbstractSource {
         id: this.#config.id || 'lsm9ds1',
         timestamp: now,
 
-        // Conversion des données brutes en unités physiques
-        // Accéléromètre en m/s^2
+        // Converting raw data to physical units
+        // Accelerometer in m/s^2
         accelerometer: {
-          x: Accel_Buffer.readInt16LE(2) * ACCEL_SENSIVITY * GRAV_TO_M_S_2, // Inversion des axes x et y
-          y: Accel_Buffer.readInt16LE(0) * ACCEL_SENSIVITY * GRAV_TO_M_S_2,
-          z: Accel_Buffer.readInt16LE(4) * ACCEL_SENSIVITY * GRAV_TO_M_S_2,
+          x: accelBuffer.readInt16LE(2) * ACCEL_SENSIVITY * GRAV_TO_M_S_2, // Swapping the x and y axes
+          y: accelBuffer.readInt16LE(0) * ACCEL_SENSIVITY * GRAV_TO_M_S_2,
+          z: accelBuffer.readInt16LE(4) * ACCEL_SENSIVITY * GRAV_TO_M_S_2,
           timestamp: now,
           frequency: 1000 / (this.#config.interval || DEFAULT_INTERVAL_MS),
         },
 
-        // Gyroscope en rad/s
+        // Gyroscope in rad/s
         gyroscope: {
-          x: Gyro_Buffer.readInt16LE(2) * GYRO_SENSIVITY * DEG_TO_RAD, // Inversion des axes x et y
-          y: Gyro_Buffer.readInt16LE(0) * GYRO_SENSIVITY * DEG_TO_RAD,
-          z: Gyro_Buffer.readInt16LE(4) * GYRO_SENSIVITY * DEG_TO_RAD,
+          x: gyroBuffer.readInt16LE(2) * GYRO_SENSIVITY * DEG_TO_RAD, // Swapping the x and y axes
+          y: gyroBuffer.readInt16LE(0) * GYRO_SENSIVITY * DEG_TO_RAD,
+          z: gyroBuffer.readInt16LE(4) * GYRO_SENSIVITY * DEG_TO_RAD,
           timestamp: now,
           frequency: 1000 / (this.#config.interval || DEFAULT_INTERVAL_MS),
         },
 
-        // Magnétomètre en uT
+        // Magnetometer in µT
         magnetometer: {
-          x: Magneto_Buffer.readInt16LE(2) * MAGNETO_SENSIVITY * GAUSS_TO_UT, // Inversion des axes x et y
-          y: Magneto_Buffer.readInt16LE(0) * MAGNETO_SENSIVITY * GAUSS_TO_UT,
-          z: Magneto_Buffer.readInt16LE(4) * MAGNETO_SENSIVITY * GAUSS_TO_UT,
+          x: magnetoBuffer.readInt16LE(2) * MAGNETO_SENSIVITY * GAUSS_TO_UT, // Swapping the x and y axes
+          y: magnetoBuffer.readInt16LE(0) * MAGNETO_SENSIVITY * GAUSS_TO_UT,
+          z: magnetoBuffer.readInt16LE(4) * MAGNETO_SENSIVITY * GAUSS_TO_UT,
           timestamp: now,
           frequency: 1000 / (this.#config.interval || DEFAULT_INTERVAL_MS),
         },
@@ -272,7 +271,7 @@ class Lsm9ds1Source extends AbstractSource {
 
       this.state.set({ frame: [data] });
 
-      //console.log('[Frame envoyé:', JSON.stringify(data, null, 2));
+      //console.log('[Frame sent:', JSON.stringify(data, null, 2));
 
     } catch (err) {
       console.error('Reading error:', err.message);
